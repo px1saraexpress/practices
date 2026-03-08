eddbb6af165f861822a8ce26e44fa571679257fa
px1saraexpress <px1saraexpress@gmail.com>
2026-03-09 03:45:30 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,77 +1,16 @@
-//# region Student Examples
+﻿
+Student s1 = new Student();
+s1.PrintFullName(); // Method call
 
-//int RollNumber = 01;
-//string Name = "Abhi";
-//string Gender = "Male";
+Student s2 = new Student();
+s2.firstName = "Amol";
+s2.lastName = "Wagh";
+s2.PrintFullName();
 
-//Console.WriteLine($"RollNumber : {RollNumber}\n" + $"Name : {Name}\n" + $"Gender : {Gender}\n" );
-
-//// Object of class
-//Student s1 = new Student();
-//s1.RollNumber = 2;
-//s1.Name = "Akash";
-//s1.Gender = "Male";
-//s1.email = "akshay@abc.com";
-//s1.contact = 78958569;
-
-//s1.Details();
-
-//Student s2 = new Student();
-//s2.RollNumber = 3;
-//s2.Name = "Asha";
-//s2.Gender = "Female";
-//s1.contact = 78947568;
-
-//s2.Details();
-
-//// Console.WriteLine($"s2 : RollNumber : {s2.RollNumber}\n" + $"s2 : Name : {s2.Name}\n" + $"s2 : Gender : {s2.Gender}\n ");
-
-//// Object initializer syntax
-
-//Student s3 = new Student()
-//{
-//    RollNumber = 4,
-//    Name = "Prakash",
-//    Gender = "Male",
-//    email = "prakash@xyz.com",
-//    contact = 78854789,
-
-//};
-
-//s3.Details();
-
-//Student s4 = new Student()
-//{
-//    RollNumber = 5,
-//    Name = "Amol",
-//    Gender = "Male",
-//    email = "prakash@xyz.com",
-//    contact = 78452858,
-
-//};
-//s4.Details();
-
-//Console.WriteLine($"Default trainer name = {Student.TrainerName}");
-//Student.Print();
-
-//#endregion Student Examples
-
-#region static class example
-
-
-
-#endregion static class example
-
-circle c1 = new circle() { Radius = 5 };
-c1.Area();
-
-circle c2 = new circle() { Radius = 7 };
-c2.Area();
-
-circle c3 = new circle() { Radius = 15 };
-c3.Area();
-
-circle c4 = new circle() { Radius = 1 };
-c4.Area();
+Student s3 = new Student()
+{ firstName = "Amol",
+  lastName = "Ningale"
+};
+s3.PrintFullName();
 
 Console.ReadLine();
--- Student.cs
+++ Student.cs
@@ -1,53 +1,17 @@
-using System.Diagnostics;
-using System.Globalization;
-
-class Student // Class 
+﻿public class Student 
 {
-    public int RollNumber; // class field
-    public string Name;    // class field
-    public string Gender;  // class field
-    public string email;   // class field
-    public int contact;
-    public static string TrainerName = "vikul";
+    public string firstName;
+    public string lastName;
 
-    public void Details()  // Method
+    // Constructor //
+    public Student()
     {
-        Console.WriteLine
-            (
-            $"RollNumber : {RollNumber}\n" + 
-            $"Name : {Name} \n" + 
-            $"Gender: {Gender} \n" + 
-            $"email : {email}\n" + 
-            $"contact : {contact}\n"+
-            $"TrainerName : {TrainerName}\n"
-            );
+        Console.WriteLine("Student () constructor called.");
     }
 
-    public static void Print()
+    public void PrintFullName()
     {
-        Console.WriteLine("static Print method called");
+        Console.WriteLine($"Full Name = {firstName} {lastName}\a");
     }
 
-
 }
-
-//////********************/////
-
-public static class India
-{
-    public static string Capital = " Delhi";
-}
-
-public class circle
-{
-    public int Radius;
-    public static float Pi = 3.14f;
-
-    public void Area()
-    {
-        Console.WriteLine($"Area of circle is = {Radius * Radius * Pi } cm");
-    }
-}
-
-
-
