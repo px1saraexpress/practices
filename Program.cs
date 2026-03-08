c254b13e6f9144b724fdb67f648021332d52b822
px1saraexpress <px1saraexpress@gmail.com>
2026-03-09 02:46:50 +0530

    Refactor student examples and add circle area calculations
    Commented out previous student examples and added circle examples.

--- Program.cs
+++ Program.cs
@@ -1,52 +1,77 @@
-﻿int RollNumber = 01;
-string Name = "Abhi";
-string Gender = "Male";
+//# region Student Examples
 
-Console.WriteLine($"RollNumber : {RollNumber}\n" + $"Name : {Name}\n" + $"Gender : {Gender}\n" );
+//int RollNumber = 01;
+//string Name = "Abhi";
+//string Gender = "Male";
 
-// Object of class
-Student s1 = new Student();
-s1.RollNumber = 2;
-s1.Name = "Akash";
-s1.Gender = "Male";
-s1.email = "akshay@abc.com";
-s1.contact = 78958569;
+//Console.WriteLine($"RollNumber : {RollNumber}\n" + $"Name : {Name}\n" + $"Gender : {Gender}\n" );
 
-s1.Details();
+//// Object of class
+//Student s1 = new Student();
+//s1.RollNumber = 2;
+//s1.Name = "Akash";
+//s1.Gender = "Male";
+//s1.email = "akshay@abc.com";
+//s1.contact = 78958569;
 
-Student s2 = new Student();
-s2.RollNumber = 3;
-s2.Name = "Asha";
-s2.Gender = "Female";
-s1.contact = 78947568;
+//s1.Details();
 
-s2.Details();
+//Student s2 = new Student();
+//s2.RollNumber = 3;
+//s2.Name = "Asha";
+//s2.Gender = "Female";
+//s1.contact = 78947568;
 
-// Console.WriteLine($"s2 : RollNumber : {s2.RollNumber}\n" + $"s2 : Name : {s2.Name}\n" + $"s2 : Gender : {s2.Gender}\n ");
+//s2.Details();
 
-// Object initializer syntax
+//// Console.WriteLine($"s2 : RollNumber : {s2.RollNumber}\n" + $"s2 : Name : {s2.Name}\n" + $"s2 : Gender : {s2.Gender}\n ");
 
-Student s3 = new Student()
-{
-    RollNumber = 4,
-    Name = "Prakash",
-    Gender = "Male",
-    email = "prakash@xyz.com",
-    contact = 78854789,
-   
-};
+//// Object initializer syntax
 
-s3.Details();
+//Student s3 = new Student()
+//{
+//    RollNumber = 4,
+//    Name = "Prakash",
+//    Gender = "Male",
+//    email = "prakash@xyz.com",
+//    contact = 78854789,
 
-Student s4 = new Student()
-{
-    RollNumber = 5,
-    Name = "Amol",
-    Gender = "Male",
-    email = "prakash@xyz.com",
-    contact = 78452858,
-    
-};
-s4.Details();
+//};
+
+//s3.Details();
+
+//Student s4 = new Student()
+//{
+//    RollNumber = 5,
+//    Name = "Amol",
+//    Gender = "Male",
+//    email = "prakash@xyz.com",
+//    contact = 78452858,
+
+//};
+//s4.Details();
+
+//Console.WriteLine($"Default trainer name = {Student.TrainerName}");
+//Student.Print();
+
+//#endregion Student Examples
+
+#region static class example
+
+
+
+#endregion static class example
+
+circle c1 = new circle() { Radius = 5 };
+c1.Area();
+
+circle c2 = new circle() { Radius = 7 };
+c2.Area();
+
+circle c3 = new circle() { Radius = 15 };
+c3.Area();
+
+circle c4 = new circle() { Radius = 1 };
+c4.Area();
 
 Console.ReadLine();
