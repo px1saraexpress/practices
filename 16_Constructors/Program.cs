3282bb93e769c29d7c24a80b2888a859a5606984
px1saraexpress <px1saraexpress@gmail.com>
2026-03-09 03:48:14 +0530

    Add files via upload

---
+++ 16_Constructors/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Student s1 = new Student();
+s1.PrintFullName(); // Method call
+
+Student s2 = new Student();
+s2.firstName = "Amol";
+s2.lastName = "Wagh";
+s2.PrintFullName();
+
+Student s3 = new Student()
+{ firstName = "Amol",
+  lastName = "Ningale"
+};
+s3.PrintFullName();
+
+Console.ReadLine();
---
+++ 16_Constructors/Student.cs
@@ -0,0 +1,17 @@
+﻿public class Student 
+{
+    public string firstName;
+    public string lastName;
+
+    // Constructor //
+    public Student()
+    {
+        Console.WriteLine("Student () constructor called.");
+    }
+
+    public void PrintFullName()
+    {
+        Console.WriteLine($"Full Name = {firstName} {lastName}\a");
+    }
+
+}
