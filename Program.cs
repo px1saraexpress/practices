a893c3441bd76ad8973e87d7db32e8c593e5fea5
px1saraexpress <px1saraexpress@gmail.com>
2026-03-09 01:58:16 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿int RollNumber = 01;
+string Name = "Abhi";
+string Gender = "Male";
+
+Console.WriteLine($"RollNumber : {RollNumber}\n" + $"Name : {Name}\n" + $"Gender : {Gender}\n" );
+
+// Object of class
+Student s1 = new Student();
+s1.RollNumber = 2;
+s1.Name = "Akash";
+s1.Gender = "Male";
+s1.email = "akshay@abc.com";
+s1.contact = 78958569;
+
+s1.Details();
+
+Student s2 = new Student();
+s2.RollNumber = 3;
+s2.Name = "Asha";
+s2.Gender = "Female";
+s1.contact = 78947568;
+
+s2.Details();
+
+// Console.WriteLine($"s2 : RollNumber : {s2.RollNumber}\n" + $"s2 : Name : {s2.Name}\n" + $"s2 : Gender : {s2.Gender}\n ");
+
+// Object initializer syntax
+
+Student s3 = new Student()
+{
+    RollNumber = 4,
+    Name = "Prakash",
+    Gender = "Male",
+    email = "prakash@xyz.com",
+    contact = 78854789,
+   
+};
+
+s3.Details();
+
+Student s4 = new Student()
+{
+    RollNumber = 5,
+    Name = "Amol",
+    Gender = "Male",
+    email = "prakash@xyz.com",
+    contact = 78452858,
+    
+};
+s4.Details();
+
+Console.ReadLine();
---
+++ Student.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+class Student // Class 
+{
+    public int RollNumber; // class field
+    public string Name;    // class field
+    public string Gender;  // class field
+    public string email;   // class field
+    public int contact;
+    public static string TrainerName = "vikul";
+
+    public void Details()  // Method
+    {
+        Console.WriteLine
+            (
+            $"RollNumber : {RollNumber}\n" + 
+            $"Name : {Name} \n" + 
+            $"Gender: {Gender} \n" + 
+            $"email : {email}\n" + 
+            $"contact : {contact}\n"+
+            $"TrainerName : {TrainerName}\n"
+            );
+    }
+
+}
