edb1989f805c0278c404bf26a2024ed9ea6bb4ed
px1saraexpress <px1saraexpress@gmail.com>
2026-03-09 16:09:30 +0530

    16 Constructor_2 updated

---
+++ 16_Constructor_2/Program.cs
@@ -0,0 +1,8 @@
+﻿
+//Student s1 = new Student();
+//Student s2 = new Student();
+//Student s3 = new Student();
+
+
+Console.WriteLine($"CourseName = {Student.CourseName}");
+Console.ReadLine();
---
+++ 16_Constructor_2/Student.cs
@@ -0,0 +1,14 @@
+﻿public class Student
+{
+    public Student()
+    {
+        Console.WriteLine("public student() constructor called");
+    }
+
+    public static string CourseName = "DotNet";
+    static Student()
+    {
+        Console.WriteLine("static student() constructor called");
+    }
+
+}
