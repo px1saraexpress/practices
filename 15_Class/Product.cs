a241767675ad9542cec94f26ec3e61c928e62899
px1saraexpress <px1saraexpress@gmail.com>
2026-03-12 00:23:56 +0530

    simplified class created

---
+++ 15_Class/Product.cs
@@ -0,0 +1,21 @@
+﻿ 
+class products
+{
+    public static string Discount = "10%"; 
+    public int productId;
+    public string productName;
+    public int productPrice;
+
+
+    public void productsDegtails()
+    {
+        Console.WriteLine
+            ($"Product Id = {productId}\n" +
+            $"Product Name = {productName}\n" +
+            $"Product price = {productPrice}\n" +
+            $"Net Discount on this product is = {Discount}\n\a"
+            );
+
+    }
+}
+
---
+++ 15_Class/Program.cs
@@ -0,0 +1,27 @@
+﻿
+products Milk = new products
+{
+    productId = 10001,
+    productName = "Milk",
+    productPrice = 30,
+};
+Milk.productsDegtails();
+
+products Sugar = new products
+{
+    productId = 10002,
+    productName = "Sugar",
+    productPrice = 42,
+};
+Sugar.productsDegtails();
+
+products Bread = new products
+{
+    productId = 10003,
+    productName = "Bread",
+    productPrice = 25,
+};
+Bread.productsDegtails();
+
+
+Console.ReadLine();
